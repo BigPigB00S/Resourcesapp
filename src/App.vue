70806893f52510fa034b3ec3b51aabc45305e0ea
BigPigB00S <bpig47575@gmail.com>
2025-06-28 00:24:50 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <template>
-  <ul></ul>
+  <ul>
+    <learning-resource
+      v-for="res in storedResources" 
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></learning-resource>
+  </ul>
 </template>
 
 <script>
+import LearningResource from './components/Learning-resources/StudyResource.vue';
   export default {
+    components: {
+      LearningResource,
+    },  
     data () {
       return {
         storedResources: [
@@ -24,3 +36,16 @@
     }
   }
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+* {
+  box-sizing: border-box;
+}
+html {
+  font-family: 'Roboto', sans-serif;
+}
+body {
+  margin: 0;
+}
+</style>
