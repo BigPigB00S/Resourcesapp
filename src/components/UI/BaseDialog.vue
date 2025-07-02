36ed512c2cc8513ffd9190e25c680abd0415db01
BigPigB00S <bpig47575@gmail.com>
2025-07-02 16:48:43 +0700

    finish

--- src/components/UI/BaseDialog.vue
+++ src/components/UI/BaseDialog.vue
@@ -1,17 +1,22 @@
 <template>
-   <dialog open>
-      <header>
-         <slot name="header">
-            <h2>{{ title }}</h2>
-         </slot>
-      </header>
-      <section>
-         <slot></slot>
-      </section>
-      <menu>
-         <slot name="actions"></slot>
-      </menu>
-   </dialog>
+   <teleport to="body">
+      <div @click="$emit('close')"></div>
+         <dialog open>
+         <header>
+            <slot name="header">
+               <h2>{{ title }}</h2>
+            </slot>
+         </header>
+         <section>
+            <slot></slot>
+         </section>
+         <menu>
+            <slot name="actions">
+               <base-button @click="$emit('close')">Close the tag</base-button>
+            </slot>
+         </menu>
+      </dialog>
+   </teleport>
 </template>
 
 <script>
@@ -21,9 +26,69 @@ export default {
          type: String,
          required: false,
          
-      }
-   }
+      },
+   },
+   emits: ['close']
+}
+</script>
+
+<style scoped>
+div {
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.75);
+  z-index: 10;
 }
 
-</script>
+dialog {
+  position: fixed;
+  top: 20vh;
+  left: 10%;
+  width: 80%;
+  z-index: 100;
+  border-radius: 12px;
+  border: none;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 0;
+  margin: 0;
+  overflow: hidden;
+}
+
+header {
+  background-color: #3a0061;
+  color: white;
+  width: 100%;
+  padding: 1rem;
+}
+
+header h2 {
+  margin: 0;
+}
+
+section {
+  padding: 1rem;
+  color: green;
+  font-weight: bold;
+
+}
+
+menu {
+  padding: 1rem;
+  display: flex;
+  justify-content: flex-end;
+  margin: 0;
+}
+
+@media (min-width: 768px) {
+  dialog {
+    left: calc(50% - 20rem);
+    width: 40rem;
+  }
+}
+
+
+</style>
 
